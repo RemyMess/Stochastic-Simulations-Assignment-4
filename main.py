7e0f2b4f27f19af5b9d302445685d2294656b9ad
remymess <remy.messadene@gmail.com>
2020-02-02 16:24:48 +0000

    finalisation of the mlmc; TODO: debug

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from pymlmc import mlmc_test
 import numpy as np
-from math import log, exp
+from math import log, exp, sqrt
 
 
-def mlmc_expectation(l, n_0, lambd, n_path=100):
+def mlmc_expectation(level, n_path, n_0=1000, ref_fac=2, lambd=2):
     """
-    :param :
-    :param :
-    :param :
-    :param :
-    :param :
-    :param :
+    Computation of expectation of X_t**2 driven by an SDE using MLMC.
 
+    :param l: level
+    :param n_0: number of path to do the expectation on for l=0
+    :param lambd: lambda parameter
+    :param ref_fac: refinement factor per level
+    :param n_path: number of paths for expectation
     :return:
-    numpy array "sums"e such that
+    numpy array "sums" such that
       sums[0] = sum(Pf-Pc)
       sums[1] = sum((Pf-Pc)**2)
       sums[2] = sum((Pf-Pc)**3)
@@ -24,48 +24,84 @@ def mlmc_expectation(l, n_0, lambd, n_path=100):
       with Pf and Pc being the estimator at the fine respectively coarse level.
     """
     # Arg checks
-    assert(l >=0 and lambd >=0)
+    assert(level >= 0 and n_path > 0 and n_0 > 0 and ref_fac > 0 and lambd > 0)
 
+    # 1. Setup (f,c, d denotes "fine/coarse" resp. )
+    # terminal time t
+    t_f = (level + 1) * log(ref_fac) / lambd
+    t_c = t_f * level / (level + 1) if level != 0 else 0
+    delta_t = t_f - t_c if level != 0 else t_f
 
+    # time mesh
+    n_f = n0 * ref_fac ** level
+    n_c = n_f / ref_fac
 
-    if l == 0:
-        # 1. Setup
-        n =
-        h =
-        t =
+    # mesh
+    h_f = 1 / n_f
+    h_c = 1 / n_c
 
-        # 2. Run
+    # placeholder for parallel values
+    if level == 0:
+        n_path = n_0
+
+    x_f = np.zeros(n_path)
+    x_c = np.zeros(n_path)
+
+    # 2. Compute fine path on delta_t
+    for _ in range(round(delta_t / h_f)):
+        dw_f = np.random.randn(1, n_path) * sqrt(h_f)
+        x_f = (1 - h_f) * x_f + dw_f
+
+    # 3. Compute both paths
+    if level != 0:
+        for _ in range(int(n_c)):
+            dw_c = np.zeros(n_path)
+
+            # Drive fine path per coarse update
+            for _ in range(ref_fac):
+                dw_f = np.random.randn(1, n_path) * sqrt(h_f)
+                x_f = (1 - h_f) * x_f + dw_f
+                # Update coarser increment
+                dw_c = dw_c + dw_f
+
+            # Compute coarse values
+            x_c = (1 - h_c) * x_c + dw_c
 
     else:
-        # 1. Setup (f,c denotes "fine/coarse" resp. )
-        # terminal time t
-        t_f = (l + 1) * log(2) / lambd
-        t_c = t_f / l
+        x_c = x_f * 0
 
-        # time mesh
-        h_f = 2 ** (-l)
-        h_c = h_f / 2
+    x_c = x_c * 2
+    x_f = x_f ** 2
 
-        # number time steps
-        n_f = 1 / h_f
-        h_c = n_f * 2
-
-        for _ in range(n_path):
-            # use itetools!!!!!!!!!!!!!!!!!!!
-            "for coarse and fine"
-                # gen bm
-                dw = np.random.randn(n_f) * sqrt(h_f)
-                x = x * (1-h_f) + dw
+    return np.array([np.sum(x_f - x_c),
+            np.sum((x_f - x_c) ** 2),
+            np.sum((x_f - x_c) ** 3),
+            np.sum((x_f - x_c) ** 4),
+            np.sum(x_f),
+            np.sum(x_f ** 2)])
 
 
-
-    pass
-
-
-# Script
+# Test expectation and compare with pymlmc package
 if __name__ == "__main__":
-    # 1. Test expectation
-    pass
+    fn = "log_compute.txt"
+    logf = open(fn, "w")
 
-    # 2. Compare with pymlmc package
-    mlmc_test()
+    # Setting params for test
+    M = 2  # Refinement factor.
+    N = 20000  # Number of paths
+    L = 5  # Levels
+    Eps = [0.05, 0.02, 0.01, 0.005, 0.002, 0.001]  # Accuracy array
+    N0 = 10  # Paths for l=0
+    Lmin = 2  # Minimum number of levels
+    Lmax = 100  # Maximum number of levels
+    n0 = 9  # Initial time steps
+
+    def test_func(L,N):
+        return mlmc_expectation(L, N, n_0=N0, ref_fac=M, lambd=2)
+
+    mlmc_test(test_func, M, N, L, N0, Eps, Lmin, Lmax, logf)
+    del logf
+
+    # Plot
+    mlmc_plot(fn, nvert=3)
+    plt.savefig(fn.replace('.txt', '.eps'))
